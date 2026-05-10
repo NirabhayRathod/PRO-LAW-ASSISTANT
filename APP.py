11d890c947dfed17bc94f4bf6b2de5f49471e5b2
NirabhayRathod <nirbhay105633016@gmail.com>
2026-05-10 18:33:19 +0530

    update App.py for streamlit cloud secrets

--- APP.py
+++ APP.py
@@ -157,7 +157,7 @@ st.markdown('<div class="sub-header">Your Intelligent Legal Research Partner •
 
 # Load environment variables
 load_dotenv()
-groq_api_key =os.getenv('GROQ_API_KEY')
+groq_api_key =st.secrets["VAR_NAME"]
 
 if not groq_api_key:
     st.error("❌ GROQ_API_KEY not found in environment variables")
